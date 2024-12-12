50aaf38978348b6ddc545ef99e85b089d8f14872
Nisal Perera <nisal.pathmila.perera@gmail.com>
2024-12-13 01:22:21 +0530

    Lint issues fixed

--- src/contexts/layoutContext.tsx
+++ src/contexts/layoutContext.tsx
@@ -1,6 +1,5 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useState } from 'react'
 import { LayoutContextTypes } from '../types/componentTypes'
-import { useWindowDimensions } from '../hooks/layoutHooks'
 
 type LayoutProps = {
   children: React.ReactNode
@@ -11,14 +10,7 @@ export const LayoutContext = createContext<LayoutContextTypes | undefined>(
 )
 
 export const LayoutProvider = ({ children }: LayoutProps) => {
-  const { windowWidth } = useWindowDimensions()
   const [showLeftPanel, setShowLeftPanel] = useState<boolean>(false)
-
-  // useEffect(() => {
-  //   if (windowWidth !== null) {
-  //     setShowLeftPanel(windowWidth > 700)
-  //   }
-  // }, [windowWidth])
 
   return (
     <LayoutContext.Provider value={{ showLeftPanel, setShowLeftPanel }}>
