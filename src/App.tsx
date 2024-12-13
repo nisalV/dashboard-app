044f31cb796732d40e7068fe094564fc5604c36d
Nisal Perera <nisal.pathmila.perera@gmail.com>
2024-12-14 01:30:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Header } from './components/views/header'
 import { LayoutProvider } from './contexts/layoutContext'
 import { Dashboard } from './components/pages/dashboard'
 
-const queryClient = new QueryClient()
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <LayoutProvider>
-        <div id="app-container">
-          <Header />
-          <div id="content-container">
-            <Dashboard />
-          </div>
+    <LayoutProvider>
+      <div id="app-container">
+        <Header />
+        <div id="content-container">
+          <Dashboard />
         </div>
-      </LayoutProvider>
-    </QueryClientProvider>
+      </div>
+    </LayoutProvider>
   )
 }
 
